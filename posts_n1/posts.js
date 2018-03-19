f958b260de6ed63245086dc9908f081b537665a9
kravchuk-s <staskravchuk@gmail.com>
2018-03-20 00:12:17 +0200

    async await check

--- posts_n1/posts.js
+++ posts_n1/posts.js
@@ -7,56 +7,101 @@ let output = '';
 
 let idOfPost = (location.href).split('?postID=', 2)[1];
 let idOfUser = (location.href).split('?userID=', 2)[1];
+let containPost = false;
+let containUser = false;
 
+renderPage();
 
-if(idOfPost){
-  postPage(idOfPost);
-}else if(idOfUser){
-  getUser(idOfUser);
-} else {
-  postList();
+async function renderPage() {
+
+  await isContainPost(idOfPost);
+  await isContainUser(idOfUser);
+
+  if (containPost) {
+    postPage(idOfPost);
+  } else if (containUser) {
+    getUser(idOfUser);
+  } else {
+    postList();
+  }
+}
+
+async function isContainPost(idOfPost) {
+  await fetch(url).then(response => response.json()).then((data) => {
+    data.forEach((post) => {
+      if (post.id == idOfPost) {
+        containPost = true;
+      }
+    });
+  }).catch((err) => console.log(err))
+}
+
+async function isContainUser(idOfUser) {
+  await fetch(userUrl).then(response => response.json()).then((data) => {
+    data.forEach((user) => {
+      if (user.id == idOfUser) {
+        containUser = true;
+      }
+    });
+  }).catch((err) => console.log(err))
 }
 
 function postPage(idOfPost) {
   fetch(url).then(response => response.json()).then((data) => {
-    const post = data[idOfPost-1] || {};
-    const {title,body,userId} = post;
-
+    const post = data[idOfPost - 1] || {};
+    const {
+      title,
+      body,
+      userId
+    } = post;
     output = `<h3> Title: <br> ${title}  </h3>
           <h4> Post: <br> ${body} </h4>
           <h3 onClick="userOnClick(${userId})" id="userID=${userId}">User: ${userId}</h3>
           `;
-
     ul.innerHTML = output;
     getComments(idOfPost);
-
   }).catch((err) => console.log(err))
 }
 
 function postList() {
   fetch(url).then(response => response.json()).then((data) => {
     data.forEach((post) => {
-      const {id} = post;
-
-      output += `<div><a id="postID=${id}" href="${location.href}?postID=${id}">Post #${id}</a></div>`;
+      const {
+        id
+      } = post;
+      output += `<div><p id="postID=${id}">Post #${id}</p></div>`;
       ul.innerHTML = output;
     });
-    
+    postOnClick();
   }).catch((err) => console.log(err))
 }
 
+function postOnClick() {
+  fetch(url).then(response => response.json()).then((data) => {
+    data.forEach((post) => {
+      const {
+        id
+      } = post;
+      document.getElementById("postID=" + id).onclick = function() {
+        location.href = "?postID=" + id;
+      };
+    });
+  }).catch((err) => console.log(err))
+}
 
 function getComments(idOfPost) {
   fetch(commentUrl).then(response => response.json()).then((data) => {
     data.forEach((comment) => {
-      const {body,name,email} = comment;
-
+      const {
+        body,
+        name,
+        email
+      } = comment;
       if (comment.postId == idOfPost) {
         output += `<p> Comment: ${body}</h3>
                    <h6> Name: ${name} </h6>
                    <h6> Email: ${email}</h6>
                   `;
-
         ul.innerHTML = output;
       }
     });
@@ -69,16 +114,20 @@ function userOnClick(userID) {
 
 function getUser(userID) {
   fetch(userUrl).then(response => response.json()).then((users) => {
-    
-    const user = users[userID-1];
-    const {name,username,email,address:{street}}=user;
-
+    const user = users[userID - 1];
+    const {
+      name,
+      username,
+      email,
+      address: {
+        street
+      }
+    } = user;
     output = `<h4> Name: ${name}  </h4>
                   <h4> User Name: ${username}  </h4>
                   <h4> Email: ${email}  </h4>
                   <h4> Address: ${street}  </h4>
                   `;
     ul.innerHTML = output;
-    
   }).catch((err) => console.log(err))
 }
