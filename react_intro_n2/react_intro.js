6d6246ff95045e5b2c6b1cb10c76bfef1c32132c
kravchuk-s <staskravchuk@gmail.com>
2018-03-23 21:42:02 +0200

    some changes

--- react_intro_n2/react_intro.js
+++ react_intro_n2/react_intro.js
@@ -1,55 +1,66 @@
  const React = {
-    createElement(elementTag, style, input) {
-      let output = '';
-      // let toText = JSON.stringify(style.style).replace(/[^a-zA-Z :]/g, "").toLowerCase();
-      // console.log(style.style);
-      // console.log(toText);
-      if (Array.isArray(input)) {
-        input.forEach((element) => {
-            console.log("here 1" + output);
-            // switch(elementTag) {
-            //   case 'div':
-            //     output += `<div>${element}</div>`
-            //     console.log("here 2" + output);
-            //     break;   
-            //   default:
-            //   output = elementTag;  
-            output += chooseTag(elementTag, element);
-          });
-        
+
+  createElement(tag, style, input) {
+    let output = '';
+    let inputOne = '';
+
+    if (Array.isArray(input)){
+        input.forEach((element) => {          
+          inputOne += element;
+        }
+          );
+      
     } else {
-      output = chooseTag(elementTag, input)
+        inputOne = input;
     }
 
-    function chooseTag(tag, elementInput) {
-      let out = '';
-      switch (tag) {
-        case 'div':
-          out = `<div>${elementInput}</div>`
-          console.log("here 2" + out);
-          break;
-        default:
-          out = elementTag;
-      }
-      return out;
+    switch(tag) {
+      case 'div':
+      output = `<div>${inputOne}</div>`
+      break;
+      case 'br':
+      output = `<br>`;
+      break;
+      case 'span':
+      output = `<span>${inputOne}</span>`;
+      break;
+      default:
+      output = tag;
     }
-    console.log("here 3" + output);
+   
     return output;
   },
   render(app, element) {
     element.innerHTML = app;
   }
  }
- const app = React.createElement('div', {
-  style: {
-    backgroundColor: 'red'
-  }
- }, [React.createElement('some text'), React.createElement('some text')]);
- // const app = 
- // React.createElement('div', { style: { backgroundColor: 'red' } }, [
- //   React.createElement('span', undefined, 'Hello world'),
- //   React.createElement('br'),
- //   'This is just a text node',
- //   React.createElement('div', { textContent: 'Text content' }),
- //   ]);
- React.render(app, document.getElementById('root'), );
+
+const kol = React.createElement('div', undefined,
+ [React.createElement('br'), React.createElement('span',undefined,'some text')]);
+
+ // const app = React.createElement('div', {  style: {    backgroundColor: 'red'  } },
+ //  [React.createElement('some text'), React.createElement('some text')]);
+
+ React.render(kol, document.getElementById('root'), );
+
+
+
+  // if (Array.isArray(input)) {
+  //     input.forEach((element) => {
+  //       output += chooseTag(elementTag, element);
+  //     });
+  //   } else {
+  //     output = chooseTag(elementTag, input)
+  //   }
+
+  //   function chooseTag(tag, elementInput) {
+  //     let out = '';
+  //     switch (tag) {
+  //       case 'div':
+  //         out = `<div>${elementInput}</div>`
+  //         break;
+  //       default:
+  //         out = elementInput;
+  //     }
+  //     return out;
+  //   }
