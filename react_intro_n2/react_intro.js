4fe341843fdf04c95c20a37c4d0c90ca9c97d4a1
kravchuk-s <staskravchuk@gmail.com>
2018-03-25 13:39:23 +0300

    save

--- react_intro_n2/react_intro.js
+++ react_intro_n2/react_intro.js
@@ -1,61 +1,40 @@
-  const React = {
-  createElement(tag, attrb, input) {
-    console.log("step 1");
-    let out;
-    switch (tag) {
-      case 'span':
-      case 'div':
-        out = document.createElement(tag);
-        console.log("step 2");
+ const React = {
+    createElement(tag, attrb, input) {
+      let out;
+      if (!(/\s/.test(tag))) {
+       out = document.createElement(tag);
         if (Array.isArray(input)) {
           input.forEach((element) => {
-            console.log("step 3 " + element);
             if (!(element.nodeType)) {
               out.innerHTML += element;
             } else {
-              console.log("step 4");
               out.appendChild(element);
             }
           })
-        } else {
+        } else if (typeof input === 'string') {
           out.textContent = input;
         }
-        break;
-      case 'br':
-        out = document.createElement(tag);
-        console.log("step 5");
-        break;
-      default:
-        console.log("step 6");
-        out.textContent = tag;
-    }
 
-    function attributes() {
-      const keys = Object.keys(attrb);
-      switch (keys[0]) {
-        case 'style':
-          for (key in attrb.style) {
-            out.style[key] = attrb.style[key];
+        if (typeof attrb === 'object') {
+        const keys = Object.keys(attrb);
+        for (key in attrb) {
+          if (keys == 'style') {
+            for (var key in attrb.style) {
+              out.style[key] = attrb.style[key];
+            }
+          } else {
+            out[key] = attrb[key];
           }
-          break;
-        case 'textContent':
-          out.textContent = attrb.textContent;
-          break;
+        }
       }
+      }
+      
+      return out;
+    },
+    render(app, element) {
+      element.appendChild(app);
     }
-
-
-    if (!(attrb === undefined)) {
-      attributes();
-    }
-
-    
-    return out;
-  },
-  render(app, element) {
-    element.appendChild(app);
-  }
- }
+   }
 
 const app = 
   React.createElement('div', { style: { backgroundColor: 'red' } }, [
